6eacab425a07c9609505b40ffa777cbc6f6a979f
Szymon Orzeł <oszymon08@gmail.com>
2016-11-28 23:05:23 +0100

    Update connect.lua

--- og-connect/connect.lua
+++ og-connect/connect.lua
@@ -1,8 +1,8 @@
 ---------------------------------------------------------------------------------
 
 --			Resource: og-connect/connect.lua
---			Author: .WhiteBlue (admin@our-game.eu)
---			Copyright (c) 2017 .WhiteBlue (admin@our-game.eu)
+--			Author: .WhiteBlue (admin@og-rpg.eu)
+--			Copyright (c) 2017 .WhiteBlue (admin@og-rpg.eu)
 
 ---------------------------------------------------------------------------------
 
